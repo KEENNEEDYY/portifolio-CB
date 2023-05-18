5da78b3c695734ca9f838e4f6b3be6d095c49ca5
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-05-18 15:45:22 -0300

    Creating Animated Text Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
+import AnimatedText from '@/components/AnimatedText';
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
 import ProfilePic from "../../public/images/profile/developer-pic-1.png";
+import Link from 'next/link';
+import { LinkArrow } from '@/components/Icons';
 
 export default function Home() {
   return (
@@ -17,12 +20,25 @@ export default function Home() {
             <div className='w-1/2'>
               <Image src={ProfilePic} alt="KennedyLeocadio" className='w-full h-auto' />
             </div>
-            <div className='w-1/2'>
-              <h1>Transformando a visão em realidade com código e design.</h1>
-              <p>
+            <div className='w-1/2 flex flex-col items-center self-center'>
+              <AnimatedText text="Transformando a visão em realidade com código e design." className='!text-4xl !text-left'/>
+              <p className='my-4 text-base font-medium' >
                 Como um habilidoso desenvolvedor full-stack, dedico-me a transformar ideias em aplicações web inovadoras.
                 Explore meus projetos e artigos mais recentes, mostrando minha experiência em React.js e desenvolvimento web.
               </p>
+              <div className='flex items-center self-start mt-2'>
+                <Link href="/dummy.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 
+                  rounded-lg text-lg front-semibold hover:bg-light hover:text-dark 
+                  border-2 border-solid border-transparent hover:border-dark
+                " download={true}>
+                  Resume <LinkArrow className={"w-6 ml-1"}/>
+                </Link>
+                <Link href="mailto:kennedy.costa.valenet@gmail.com" target={"_blank"} 
+                  className="ml-4 text-lg font-medium capitalize text-dark underline"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
