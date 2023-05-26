f7b0ae9fecd3b1d73e7361d7b0b7f3b958971789
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-05-26 12:21:52 -0300

    Creating Experience & Education Component

---
+++ src/components/Experience.js
@@ -0,0 +1,96 @@
+import React, { useRef } from 'react';
+import { useScroll,motion } from 'framer-motion';
+import LiIcon from './LiIcon';
+
+const Details = ({ position, company, companyLink, time, address, work}) => {
+    const ref = useRef(null);
+    return (
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between' >
+        <LiIcon reference={ref} />
+        <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}} >
+            <h3 className='capitalize font-bold text-2xl' >
+                {position}&nbsp;<a href={companyLink} 
+                target="_blank"
+                className='text-primary capitalize'
+                >
+                    @{company}
+                </a>
+            </h3>
+            <span className='capitalize font-medium text-dark/75' >
+                {time} | {address}
+            </span>
+            <p className='font-medium w-full' >
+                {work}
+            </p>
+        </motion.div>
+    </li>
+    );  
+};
+
+const Experience = () => {
+    
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll(
+        {
+            target: ref,
+            offset: ["start end", "center start"],
+        }
+    );
+
+  return (
+    <motion.div className='my-64'>
+        <h2 className='font-bold text-8xl mb-32 w-full text-center' >
+            Experience
+        </h2>
+        
+        <div ref={ref} className="w-[75%] mx-auto relative" >
+
+            <motion.div style={{scaleY: scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top" />
+            <ul className="w-full flex flex-col items-start justify-between ml-4" >
+                    <Details 
+                        position={"Analista de Redes Pleno"}
+                        company={"VALENET"}
+                        companyLink={"www.valenet.com.br"}
+                        time={"out/2021 - atual"}
+                        address={"Itabira - MG"}
+                        work={"Como analista de redes pleno, tenho uma sólida experiência em diversas áreas, incluindo o trabalho com sistemas de monitoramento, como Solar Winds, Zabbix, Grafana e Cacti. Fui responsável por configurar parâmetros essenciais, como SNMP, OID e ICMP, e por desenhar topologias para monitorar ativos de rede. Tenho habilidades sólidas em OSI/TCP IP, o que me permite identificar e solucionar incidentes de rede, aplicando soluções iniciais nas camadas física, de enlace e de rede. Na camada de enlace, possuo experiência em configurar e resolver problemas, incluindo loops de rede, gerenciamento de equipamentos com VLANs e configuração de MPLS. Tenho conhecimento em identificar interfaces, como SFP e QSFP, e em entender diferentes codificações ópticas, como multimode, singlemode, bidirecional e unidirecional. Além disso, sou capaz de analisar o sinal de equipamentos de fibra ou rádio. Tenho vivência em configurar e estruturar sistemas de telefonia fixa comutada, monitorando e analisando troncos de telefonia. Também configurei equipamentos TDM e bundle para entrega de troncos de telefonia até uma gateway. Na camada de rede, tenho experiência em DHCP, roteamento estático, OSPF, MPLS, PPPoE, NAT e firewall no Mikrotik e no Linux, utilizando IPtables. Além disso, possuo conhecimento em PABX Elastic e Isabel, o que adiciona mais habilidades ao meu perfil como analista de redes."}                
+                    />
+                    <Details 
+                        position={"Analista de Redes Junior"}
+                        company={"VALENET"}
+                        companyLink={"www.valenet.com.br"}
+                        time={"mar/2018 - out/2021"}
+                        address={"Itabira - MG"}
+                        work={"Como analista de redes júnior, fui responsável por gerenciar e oferecer suporte em redes MAN/WAN TCP/IPv4. Trabalhei com switches, roteadores e OLTs de fabricantes renomados, como ZTE, Juniper, DATACOM, Mikrotik, HUAWEI e CISCO. Além disso, realizei acesso e suporte a rádios de frequência licenciada, como SIAE/CERAGON, e rádios Ubiquit 5.8. Também forneci suporte a serviços hospedados em ambientes Linux e monitorei as redes utilizando ferramentas como Cacti e SolarWinds. Utilizei tecnologias como VLAN, switching, roteamento estático, GPON, PPPoE, SNMP e ICMP. Essas experiências foram desempenhadas em um nível júnior, evidenciando minha familiaridade e competência nessas tecnologias e protocolos de rede."}                
+                    />
+                    <Details 
+                        position={"Analista de Suporte Técnico"}
+                        company={"VALENET"}
+                        companyLink={"www.valenet.com.br"}
+                        time={"fev/2017 - mar/2018"}
+                        address={"Itabira - MG"}
+                        work={"Como analista de suporte técnico em um call center, meu papel consistia em ajudar os clientes da empresa a resolver problemas relacionados a equipamentos. Por meio de comunicação telefônica, identificava falhas nos dispositivos, bem como situações de mau uso ou falta de conhecimento por parte dos clientes. Com base nas informações coletadas, encaminhava os reparos necessários, orientava sobre o uso adequado dos equipamentos ou fornecia instruções para solucionar o problema. Meu objetivo era garantir a satisfação dos clientes, oferecendo um suporte eficiente e eficaz, contribuindo para a resolução de questões técnicas e proporcionando uma experiência positiva."}                
+                    />
+                    <Details 
+                        position={"Técnico de informática"}
+                        company={"Comunidade Educativa CEDAC"}
+                        companyLink={"comunidadeeducativa.org.br"}
+                        time={"fev/2014 - mar/2015"}
+                        address={"Itabira - MG"}
+                        work={"Fui responsável pela implementação e administração de um sistema de controle para a biblioteca, semelhante a um sistema de aluguel. Além disso, conduzi atividades de inclusão digital para a comunidade local e forneci suporte de TI aos professores da secretaria de educação. Também participei na organização e manutenção dos espaços públicos, como a biblioteca e o laboratório de informática. Essa experiência me proporcionou habilidades em administração de sistemas, suporte técnico e trabalho em equipe."}                
+                    />
+                    <Details 
+                        position={"Instrutor de informática"}
+                        company={"Compuway"}
+                        companyLink={"www.compuway.com.br"}
+                        time={"nov/2010 - jun/2014"}
+                        address={"Itabira - MG"}
+                        work={"No Laboratório de Informática, tive a responsabilidade de acompanhar os alunos em suas atividades nas diversas áreas da tecnologia. Além disso, ministrei cursos de PHP, HTML e Java para os alunos, proporcionando-lhes conhecimentos fundamentais nessas linguagens de programação. Realizei também a manutenção preventiva e corretiva de computadores, garantindo o funcionamento adequado dos equipamentos. Ainda, cuidei da manutenção da rede de informática, assegurando sua operação eficiente. No que diz respeito ao servidor Windows, fui encarregado de sua configuração e administração, buscando proporcionar um ambiente de trabalho seguro e produtivo. Ao desempenhar essas funções, adquiri amplo conhecimento em hardware, software e redes, além de desenvolver habilidades sólidas em solução de problemas e gerenciamento de servidores."}                
+                    />
+                </ul> 
+        </div>
+    </motion.div>
+  )
+}
+
+export default Experience;
--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,8 @@ import React, { useEffect, useRef } from 'react';
 import profilePic from "../../public/images/profile/developer-pic-2.jpg";
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
 import Skills from '@/components/Skills';
+import Experience from '@/components/Experience';
+import Education from '@/components/Education';
 
 const AnimatedNumbers = ({value}) => {
     const ref = useRef(null);
@@ -40,29 +42,30 @@ const about = () => {
         </Head>
         <main className="flex w-full flex-col items-center justify-center">
             <Layout className='pt-16'>
-                <AnimatedText text="Passion Fuels purpose!" className='mb-16 !text-6xl '/>
+                <AnimatedText text="A paixão é o que alimenta o propósito!" className='mb-16 !text-4xl '/>
                 <div className='grid w-full grid-cols-8 gap-16'>
                     <div className="col-span-3 flex flex-col items-start justify-start">
                         <h2 className="mb-4 text-light font-bold uppercase text-dark/75">Biografia</h2>
-                        <p className="font-medium" >
-                            Hi, I'm Kennedy, a fullstack developer and UI/UX designer with a passion for creating beautiful, functional, 
-                            and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                            new and innovative ways to bring my clients' visions to life.
+                        <p className="font-medium text-sm text-justify" >
+                            Olá, sou Kennedy, um desenvolvedor fullstack com conhecimento em implementação de projetos funcionais e 
+                            focados na usabilidade. Tenho habilidades sólidas em desenvolvimento de frontend e backend, utilizando 
+                            tecnologias como TypeScript, React, Java e Spring Boot.
                         </p>
-                        <p className="my-4 font-medium" >
-                            I believe that design is about more than just making things look pretty – it's about solving problems and 
-                            creating intuitive, enjoyable experiences for users. 
+                        <p className="my-4 font-medium text-sm text-justify" >
+                            Quando estou trabalhando em um projeto, meu foco é garantir que a funcionalidade seja priorizada e que a 
+                            usabilidade seja considerada em cada etapa. Estou constantemente buscando maneiras de melhorar minhas habilidades 
+                            e aprender novas tecnologias para oferecer soluções de qualidade e inovadoras.
                         </p>
-                        <p className="font-medium" >
-                            Whether I'm working on a website, mobile app, or 
-                            other digital product, I bring my commitment to design excellence and user-centered thinking to 
-                            every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+                        <p className="font-medium text-sm text-justify" >
+                            Estou entusiasmado fazendo uma transição para a área de desenvolvimento e estou pronto para enfrentar novos desafios. 
+                            Mal posso esperar para contribuir com minha paixão pelo desenvolvimento  e minha ênfase na funcionalidade e usabilidade de sistemas. 
+                            Meu foco e interesse é na entrega de soluções de qualidade.
                         </p>
                     </div>
                     <div className=' col-span-3 relative rounded-2xl border-2 border-solid border-dark 
                         bg-light p-8' >
                         <div className=' absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark' />
-                        <Image src={profilePic} alt='Kennedy' className='w-full h-auto rounded-2xl' />
+                        <Image src={profilePic} alt='Kennedy' className='w-full h-auto rounded-2xl mt-[5%]' />
                     </div>
                     <div className='col-span-2 flex flex-col items-end justify-between' >
                         <div className='flex flex-col items-end justify-center' >
@@ -92,6 +95,8 @@ const about = () => {
                     </div>
                 </div>
                 <Skills />
+                <Experience />
+                <Education />
             </Layout>
         </main>
     </>
