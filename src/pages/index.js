6f22d9b41f60f026bd7b92c8e4024df946ddd91f
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-06-10 09:26:14 -0300

    Make it Responsive - Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,30 +16,35 @@ export default function Home() {
         <meta name="description" content="Desenvolvedor Kennedy" />
       </Head>
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light'>
-        <Layout className='pt-0'>
-          <div className="flex items-center justify-between w-full mb-6">
-            <div className='w-1/2'>
-              <Image src={ProfilePic} alt="KennedyLeocadio" className='w-full h-auto' 
+        <Layout className='!pt-0 md:!pt-12 sm:!pt-6'>
+          <div className="flex items-center justify-between mb-6 w-full lg:flex-col">
+            <div className='w-1/2 md:w-full'>
+              <Image src={ProfilePic} alt="KennedyLeocadio" 
+                className='w-full h-auto lg:hidden md:inline-block md:w-full' 
                 priority 
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
-            <div className='w-1/2 flex flex-col items-center self-center'>
-              <AnimatedText text={`Transformando ideias em insights, códigos em soluções.`} className='!text-4xl !text-left'/>
-              <p className='my-4 text-base font-medium' >
+            <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+              <AnimatedText text={`Transformando ideias em insights, códigos em soluções.`} 
+                className='!text-4xl !text-left xl:!text-3xl lg:!text-center lg:!text-4xl md:!text-3xl sm:!text-2xl'
+              />
+              <p className='my-4 text-base font-medium lg:text-justify md:text-sm sm:text-xs' >
                 Como um desenvolvedor full-stack, dedico-me a transformar ideias em aplicações inovadoras.
                 Explore meus projetos e artigos mais recentes, mostrando minha experiência em React, SpringBoot e desenvolvimento de sistemas.
               </p>
-              <div className='flex items-center self-start mt-2'>
+              <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/dummy.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 
                   rounded-lg text-lg front-semibold hover:bg-light hover:text-dark 
                   border-2 border-solid border-transparent hover:border-dark 
                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
+                  md:p-2 md:px-4 md:text-base
                 " download={true}>
                   Currículo <LinkArrow className={"w-6 ml-1"}/>
                 </Link>
                 <Link href="mailto:kennedy.costa.valenet@gmail.com" target={"_blank"} 
-                  className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light"
+                  className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light
+                  md:text-base"
                 >
                   Contato
                 </Link>
@@ -48,7 +53,7 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
-        <div className="absolute right-8 bottom-8 inline-block w-24" >
+        <div className="absolute right-8 bottom-8 inline-block w-24 md:hidden" >
           <Image src={lightBulb} alt="Kennedy" className='w-full h-auto' />
         </div>
       </main>
