f037e4b70fc5dfac3b8ebff88f0f1c8e10b14e3a
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2023-06-12 01:10:59 -0300

    Make it Responsive Complete

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -46,7 +46,7 @@ const MovingImg = ({title, img, link}) => {
                 style={{x:x, y:y }}
                 whileInView={{opacity:1, transition:{duartion: 0.2}}}
                 initial={{opacity:0}}
-                ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute rounded-lg" />
+                ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden" />
         </Link>
     )
 }
@@ -59,10 +59,13 @@ const Article = ({img, title, date, link}) => {
             viewport={{once: true}}
             className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between 
             bg-light text-dark dark:bg-dark dark:text-light
-            first:mt-0 border border-solid border-dark dark:border-light border-r-4 border-b-4"
+            first:mt-0 border border-solid border-dark dark:border-light border-r-4 border-b-4 
+            sm:flex-col"
         >
             <MovingImg title={title} img={img} link={link} />
-            <span className="text-primary dark:text-primaryDark font-semibold pl-4" >{date}</span>
+            <span className="text-primary dark:text-primaryDark font-semibold pl-4 sm:self-start sm:pl-0 xs:text-sm" >
+                {date}
+            </span>
         </motion.li>
     )
 }
@@ -87,7 +90,11 @@ const FeaturedArticle = ({img, title, time, summary, link}) =>{
                 />
             </Link>
             <Link href={`${link}`} target="_blank">
-                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline' >{title}</h2>
+                <h2 className='capitalize text-2xl font-bold my-2 mt-4 hover:underline 
+                    xs:text-lg' 
+                >
+                    {title}
+                </h2>
             </Link>
             <p className='text-sm mb-2' >{summary}</p>
             <span className='text-primary dark:text-primaryDark font-semibold' >
@@ -106,8 +113,10 @@ const articles = () => {
         </Head>
         <main className='w-full mb-16 flex flex-col items-center overflow-hidden dark:text-light' >
             <Layout className='pt-16' >
-                <AnimatedText text="Words can change the world" className='mb-16 text-6xl ' />
-                <ul className='grid grid-cols-2 gap-16' >
+                <AnimatedText text="Words can change the world" className='mb-16 text-6xl 
+                    sm:mb-8 sm:!text-6xl xs:!text-4xl' 
+                />
+                <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16' >
                     <FeaturedArticle 
                       title="Build a Custom Pagination Component In Reactjs From Scratch"
                       summary="Learn how to build a custom pagination component in Reactjs from Scratch. Follow this step-by-step guide to integrate Paginantion component in your ReactJS project."
